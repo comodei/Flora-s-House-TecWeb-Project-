6ac66223f006f83f6e642538aea58099f20f1749
DanieleSpigolon <72398566+DanieleSpigolon@users.noreply.github.com>
2020-12-28 11:39:27 +0100

    Galleria: separazione struttura e presentazione; separazione tra comportamento e struttura;

--- javascript/js_galleria.js
+++ javascript/js_galleria.js
@@ -1,17 +1,57 @@
-// JavaScript Document
 var slideIndex = 1;
 showSlides(slideIndex);
+window.onload = scorriLink(); slide();
+function slide(){
+  var links = document.getElementsByTagName("span");
+  for (var i=0; i<links.length; i++) {
+  if (links[i].getAttribute("id") == "dot1") {
+    links[i].onclick = function() {
+      currentSlide(1);
+      return false;}
+  }
+  if (links[i].getAttribute("id") == "dot2") {
+    links[i].onclick = function() {
+      currentSlide(2);
+      return false;}
+  }
+  if (links[i].getAttribute("id") == "dot3") {
+    links[i].onclick = function() {
+      currentSlide(3);
+      return false;}
+  }
+  if (links[i].getAttribute("id") == "dot4") {
+    links[i].onclick = function() {
+      currentSlide(4);
+      return false;}
+  }
+  }
+}
+function scorriLink(){
+var links = document.getElementsByTagName("a");
+for (var i=0; i<links.length; i++) {
+  if (links[i].getAttribute("class") == "next") {
+    links[i].onclick = function() {
+      plusSlides(+1);
+      return false;}
 
+  }
+  if (links[i].getAttribute("class") == "prev") {
+    links[i].onclick = function() {
+      plusSlides(-1);
+      return false;}
+}
+}
+}
+
+// JavaScript Document
 // Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
-
 // Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
-
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
@@ -27,3 +67,4 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block"; 
   dots[slideIndex-1].className += " active";
 }
+
