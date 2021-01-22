628246b7e5d751e5667689e8248a54b3d69b1cdc
Valton <tahirajvalton@gmail.com>
2021-01-22 14:49:42 +0100

    Regex gestione prenotazioni

--- php/modificaPrenotazione.php
+++ php/modificaPrenotazione.php
@@ -1,6 +1,7 @@
 <?php
     require_once "session.php";
     require_once "connection.php";
+	require_once "input_check.php";
 
     if($_SESSION['connesso']==false){
         header('location:accessdenied.php');
@@ -25,11 +26,9 @@
 		}
 		
 		$connessione = new connection();
-		$error = '<div class="err"><ul>';
 		if($connessione->isConnected()){
 		
-			if(isset($_POST['submitDel'])) {
-				
+			if(isset($_POST['submitDel'])) {		 	
 			 $query = "DELETE FROM prenotazione WHERE Codice=\"$codice\"";
 			 $queryResult = mysqli_query($connessione->getConnection(), $query);
 			 if(mysqli_affected_rows($connessione->getConnection())>=1){
@@ -45,21 +44,40 @@
 			
 			 
 			} else {
-			
-				$query = "UPDATE prenotazione SET DataCheckIn=\"$checkin\",DataCheckOut=\"$checkout\",Richieste=\"$richieste\" WHERE Codice=\"$codice\"";
-				$queryResult = mysqli_query($connessione->getConnection(), $query);
-				if(mysqli_affected_rows($connessione->getConnection())>=1){
-					$connessione->closeConnection();
-					echo "<div class='mess'>Prenotazione aggiornata con successo</div>";
+				$querable=true;
+				$errorino="<div class='err'><ul>";
+				if(!input_check::date_check($checkin)){
+					$errorino.="<li>Inserisci una data di check-in valida: deve essere in formato ANNO-MESE-GIORNO (AAAA-MM-GG)</li>";
+					$querable=false;
+				}
+				if(!input_check::date_check($checkout)){
+					$errorino.='<li>Inserisci una data di check-out valida: deve essere in formato ANNO-MESE-GIORNO (AAAA-MM-GG)</li>';
+					$querable=false;
+				}
+				if($checkin > $checkout){
+					$errorino.='<li>La data di check in non può essere dopo la data di check out</li>';
+					$querable=false;
+				}
+				if ($querable){
+					$query = "UPDATE prenotazione SET DataCheckIn=\"$checkin\",DataCheckOut=\"$checkout\",Richieste=\"$richieste\" WHERE Codice=\"$codice\"";
+					$queryResult = mysqli_query($connessione->getConnection(), $query);
+					if(mysqli_affected_rows($connessione->getConnection())>=1){
+						$connessione->closeConnection();
+						echo "<div class='mess'>Prenotazione aggiornata con successo</div>";
+						header( "refresh:5;url=gestPrenotazioni.php" );
+						exit;
+					}
+					else {
+						echo "<div class='err'>Errore nell'aggiornamento della prenotazione </div>";
+						header( "refresh:5;url=gestPrenotazioni.php" );
+						exit;
+					}
+				}	else{
+					echo $errorino."</ul></div>";
 					header( "refresh:5;url=gestPrenotazioni.php" );
 					exit;
+					
 				}
-				else {
-					echo "<div class='err'>Errore nell'aggiornamento della prenotazione </div>";
-					header( "refresh:5;url=gestPrenotazioni.php" );
-					exit;
-				}
-				   	
 			}	
 			
 			
